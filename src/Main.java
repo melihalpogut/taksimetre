d1a3b14d66f14ae0e936b45fbe76ae870d079090
melihalpogut <melihalpagut@gmail.com>
2023-08-20 20:22:24 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int km;
+        double perKm = 2.20, total = 10;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Mesafeyi km cinsi girin : ");
+        km = input.nextInt();
+        total += (km * perKm);
+        total = (total < 20) ? 20 : total;
+        System.out.println("Toplam tutar : " + total);
+    }
+}
